ba92a3098ee79e87bf0668ba930e4bd9b3ce59b8
Tiago R <metal@i0.tf>
2023-08-25 13:06:22 +0000

    logggggg
    Signed-off-by: GitHub <noreply@github.com>

--- migrate/migrate.mjs
+++ migrate/migrate.mjs
@@ -2,21 +2,6 @@ import * as fs from 'fs';
 import fetch from 'node-fetch';
 const {MIGRATE_URL, DB_FILE} = process.env;
 
-// https://stackoverflow.com/a/22907134
-function download(url, dest) {
-  const file = fs.createWriteStream(dest);
-  http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-      console.info("Downloaded file!")
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    console.error(err.message);
-  });
-};
-
 const downloadFile = (async (url, path) => {
   const res = await fetch(url);
   const fileStream = fs.createWriteStream(path);
@@ -29,7 +14,8 @@ const downloadFile = (async (url, path) => {
 
 if(MIGRATE_URL?.length > 5 && DB_FILE?.includes("data.sqlite")) {
   console.info("Downloading file...");
-  downloadFile(MIGRATE_URL, DB_FILE)
+  await downloadFile(MIGRATE_URL, DB_FILE);
+  console.info("Done!");
 } else {
   console.warn("Skipping migration...")
   console.log(process.env.MIGRATE_URL, process.env.DB_FILE);
