92cec5c219f25d439d1c829f519dd8f801265849
Tiago R <metal@i0.tf>
2023-08-25 12:06:19 +0000

    test
    Signed-off-by: GitHub <noreply@github.com>

--- migrate.sh
+++ migrate.sh
@@ -2,11 +2,11 @@
 echo "1st parameter = $1 "
 echo "2nd Parameter = $2 "
 
-if [ -f "$1" ]; then
-  echo "SQLite data file exists"
-else
-  if [ -n "$2" ]; then
-    wget -O data.sqlite "$2"
+#if [ -f "$1" ]; then
+#  echo "SQLite data file exists"
+#else
+#  if [ -n "$2" ]; then
+    wget -O "$1" "$2"
     echo "Downloaded data file"
-  fi
-fi
+#  fi
+#fi
