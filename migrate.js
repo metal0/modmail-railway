4226a57963ffaf5acb043c308e2a85f00a2941a9
Tiago R <metal@i0.tf>
2023-08-25 12:36:47 +0000

    deez
    Signed-off-by: GitHub <noreply@github.com>

--- migrate.js
+++ migrate.js
@@ -3,20 +3,22 @@ const http = require('http');
 const {MIGRATE_URL, DB_FILE} = process.env;
 
 // https://stackoverflow.com/a/22907134
-function download(url, dest, cb) {
+function download(url, dest) {
   const file = fs.createWriteStream(dest);
   http.get(url, function(response) {
     response.pipe(file);
     file.on('finish', function() {
       file.close(cb);  // close() is async, call cb after close completes.
+      console.info("Downloaded file!")
     });
   }).on('error', function(err) { // Handle errors
     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
+    console.error(err.message);
   });
 };
 
 
 if(MIGRATE_URL?.length > 5 && DB_FILE) {
+  console.info("Downloading file...");
   download(MIGRATE_URL, DB_FILE)
 }
