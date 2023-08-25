e907df7c878b7799e9eca592cae4b3bda85437e2
Tiago R <metal@i0.tf>
2023-08-25 12:53:21 +0000

    homie...
    Signed-off-by: GitHub <noreply@github.com>

--- migrate/migrate.js
+++ migrate/migrate.js
@@ -1,16 +1,5 @@
 const fs = require("fs");
-import fetch, {
-  Headers,
-  Request,
-  Response,
-} from 'node-fetch'
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch
-  globalThis.Headers = Headers
-  globalThis.Request = Request
-  globalThis.Response = Response
-}
+const fetch = require("node-fetch");
 const {MIGRATE_URL, DB_FILE} = process.env;
 
 // https://stackoverflow.com/a/22907134
